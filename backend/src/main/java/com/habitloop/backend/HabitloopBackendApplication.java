c355bc5eca4ebc9ef9d7432383ec52e21f657d37
Andy <u1987276@campus.udg.edu>
2025-04-05 12:53:38 +0200

    init spring boot project

---
+++ backend/src/main/java/com/habitloop/backend/HabitloopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.habitloop.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitloopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitloopBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/habitloop/backend/HabitloopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habitloop.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitloopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
